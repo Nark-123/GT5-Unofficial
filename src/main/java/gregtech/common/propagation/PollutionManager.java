a60bb223ddf877574518b314afaa95cd62675101
Nark <igor.zhigalof@gmail.com>
2026-09-22 01:41:20 +0300

    Added spatial index

--- src/main/java/gregtech/common/propagation/PollutionManager.java
+++ src/main/java/gregtech/common/propagation/PollutionManager.java
@@ -2,14 +2,14 @@ package gregtech.common.propagation;
 
 import net.minecraft.util.Vec3;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PollutionManager implements PropagationManager {
 
     private final List<PropagationEmitter> emitters = new ArrayList<>();
     private final List<PropagationInfluencer> influencers = new ArrayList<>();
 
+    private final PropagationSpatialIndex spatialIndex = new PropagationSpatialIndex();
 
     @Override
     public void registerEmitter(PropagationEmitter emitter) {
@@ -18,6 +18,7 @@ public class PollutionManager implements PropagationManager {
         }
 
         emitters.add(emitter);
+        spatialIndex.add(emitter);
 
         for (PropagationInfluencer influencer : influencers) {
             if (canInfluence(emitter, influencer)) {
@@ -29,38 +30,34 @@ public class PollutionManager implements PropagationManager {
     @Override
     public void unregisterEmitter(PropagationEmitter emitter) {
         emitters.remove(emitter);
+        spatialIndex.remove(emitter);
     }
-
 
     @Override
     public void registerInfluencer(PropagationInfluencer influencer) {
-
         if (influencers.contains(influencer)) {
             return;
-        }
+        };
 
         influencers.add(influencer);
 
         for (PropagationEmitter emitter : emitters) {
-
             if (canInfluence(emitter, influencer)) {
                 emitter.addInfluencer(influencer);
             }
         }
     }
 
-
     @Override
     public void unregisterInfluencer(PropagationInfluencer influencer) {
         influencers.remove(influencer);
     }
 
-
     @Override
     public float getPollution(Vec3 pos) {
         float result = 0.0F;
 
-        for (PropagationEmitter emitter : emitters) {
+        for (PropagationEmitter emitter : spatialIndex.get(pos)) {
 
             Vec3 emitterPos = emitter.getPosition();
 
@@ -75,12 +72,11 @@ public class PollutionManager implements PropagationManager {
             InfluenceVector vector =
                 new InfluenceVector(emitterPos, pos);
 
-
-            for (PropagationInfluencer influencer : influencers) {
+            for (PropagationInfluencer influencer : emitter.getInfluencers()) {
                 influence *= influencer.influence(pos, vector);
             }
 
-            result += influence;
+            result = (float) (result + influence);
         }
 
         return result;
@@ -93,9 +89,7 @@ public class PollutionManager implements PropagationManager {
 
     private boolean canInfluence(PropagationEmitter emitter, PropagationInfluencer influencer) {
         double range = emitter.getPropagationRange() + influencer.getRange();
-
         double distance = emitter.getPosition().distanceTo(influencer.getPosition());
-
         return distance <= range;
     }
 }
---
+++ src/main/java/gregtech/common/propagation/PropagationSpatialIndex.java
@@ -0,0 +1,98 @@
+package gregtech.common.propagation;
+
+import net.minecraft.util.Vec3;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PropagationSpatialIndex {
+
+    private static final int BATCH_SIZE = 64;
+
+    private static final Comparator<Vec3> BATCH_COMPARATOR = new Comparator<Vec3>() {
+        @Override
+        public int compare(Vec3 a, Vec3 b) {
+            int x = Double.compare(a.xCoord, b.xCoord);
+            if (x != 0) return x;
+
+            int y = Double.compare(a.yCoord, b.yCoord);
+            if (y != 0) return y;
+
+            return Double.compare(a.zCoord, b.zCoord);
+        }
+    };
+
+    private final Map<Vec3, List<PropagationEmitter>> batches = new TreeMap<>(BATCH_COMPARATOR);
+
+
+    public void add(PropagationEmitter emitter) {
+        Vec3 pos = emitter.getPosition();
+        double range = emitter.getPropagationRange();
+
+        Vec3 min = getBatchPosition(Vec3.createVectorHelper(
+            pos.xCoord - range,
+            pos.yCoord - range,
+            pos.zCoord - range
+        ));
+
+        Vec3 max = getBatchPosition(Vec3.createVectorHelper(
+            pos.xCoord + range,
+            pos.yCoord + range,
+            pos.zCoord + range
+        ));
+
+        for (int x = (int) min.xCoord; x <= (int) max.xCoord; x++) {
+            for (int y = (int) min.yCoord; y <= (int) max.yCoord; y++) {
+                for (int z = (int) min.zCoord; z <= (int) max.zCoord; z++) {
+                    getOrCreate(Vec3.createVectorHelper(x, y, z)).add(emitter);
+                }
+            }
+        }
+    }
+
+
+    public void remove(PropagationEmitter emitter) {
+        for (java.util.Iterator<Map.Entry<Vec3, List<PropagationEmitter>>> iterator = batches.entrySet().iterator(); iterator.hasNext();) {
+            Map.Entry<Vec3, List<PropagationEmitter>> entry = iterator.next();
+
+            entry.getValue().remove(emitter);
+
+            if (entry.getValue().isEmpty()) {
+                iterator.remove();
+            }
+        }
+    }
+
+
+    public List<PropagationEmitter> get(Vec3 position) {
+        List<PropagationEmitter> result = batches.get(getBatchPosition(position));
+
+        if (result == null) {
+            return java.util.Collections.emptyList();
+        }
+
+        return result;
+    }
+
+
+    private List<PropagationEmitter> getOrCreate(Vec3 position) {
+        return batches.computeIfAbsent(position, k -> new ArrayList<>());
+    }
+
+
+    private Vec3 getBatchPosition(Vec3 pos) {
+        return Vec3.createVectorHelper(
+            getBatchCoordinate(pos.xCoord),
+            getBatchCoordinate(pos.yCoord),
+            getBatchCoordinate(pos.zCoord)
+        );
+    }
+
+
+    private int getBatchCoordinate(double coordinate) {
+        return (int) Math.floor(coordinate / BATCH_SIZE);
+    }
+}
